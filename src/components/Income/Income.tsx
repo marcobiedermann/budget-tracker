3a2f03dde55ba24eca1ae57e95c661808a314149
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com.>
2024-07-01 12:04:56 +0200

    feat: Form
    Add a form to enter income and expenses

---
+++ src/components/Income/Income.tsx
@@ -0,0 +1,46 @@
+import { useFieldArray, useFormContext } from 'react-hook-form';
+
+interface Income {
+  label: string;
+  value: number;
+}
+
+interface FieldValues {
+  income: Income[];
+}
+
+function Income() {
+  const { control, register } = useFormContext<FieldValues>();
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'income',
+  });
+
+  return (
+    <section>
+      <h2>Income</h2>
+      <ul>
+        {fields.map((field, index) => (
+          <li key={field.id}>
+            <label htmlFor={field.id}>{field.label}</label>
+            <input
+              id={field.id}
+              type="number"
+              {...register(`income.${index}.value`, {
+                valueAsNumber: true,
+              })}
+            />
+            <button type="button" onClick={() => remove(index)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+      <button type="button" onClick={() => append({ label: 'Income', value: 0 })}>
+        Add
+      </button>
+    </section>
+  );
+}
+
+export default Income;
